7600817b1d7eaa52e987e3651ac6f81002215906
JFTavares <fernando@booknando.com.br>
2020-02-06 13:28:55 -0300

    Novos ajustes

--- optimiza.py
+++ optimiza.py
@@ -6,11 +6,13 @@ def resize_image(file_name):
     basewidth = 1600
     file_imagem = Image.open(file_name)
     x, y = file_imagem.size
-    wpercent = (basewidth/float(file_imagem.size[0]))
-    hsize = int((float(file_imagem.size[1])*float(wpercent)))
-    nova_imagem = file_imagem.resize((basewidth,hsize), Image.ANTIALIAS)
+    if x == 1600:
+        print('Imagem já otimizada: ', file_name)
+    else:
+        wpercent = (basewidth/float(file_imagem.size[0]))
+        hsize = int((float(file_imagem.size[1])*float(wpercent)))
+        nova_imagem = file_imagem.resize((basewidth,hsize), Image.ANTIALIAS)
 
-
-    dpi = (300, 300)
-    nova_imagem.save(file_name, dpi=dpi)
-    print('Salvando a imagem ', file_name)
+        dpi = (300, 300)
+        nova_imagem.save(file_name, dpi=dpi)
+        print('Salvando a imagem ', file_name)
